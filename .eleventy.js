71b5d55b3c4ff64a6ab1faa6f9f7e411cb34bde2
Shubham <mohanty.shubham007@gmail.com>
2024-11-18 21:06:13 -0500

    feat: pass the ghost CMS data as a collection we're essentially returning the chapters

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,18 @@
-const fs = require("fs");
+// const fs = require("fs");
+import ghostData from "./_data/ghost.js";
 
-module.exports = function (eleventyConfig) {
+export default function (eleventyConfig) {
     // Pass styles for public dir
     eleventyConfig.addPassthroughCopy("src/styles");
 
     // Pass img folder for public dir
     eleventyConfig.addPassthroughCopy("src/images");
+
+    // Add a custom collection for Ghost CMS data
+    eleventyConfig.addCollection("chapters", async function () {
+        const chapters = await ghostData();
+        return chapters;
+    });
 
     return {
         // explicity state that everything should be processed using nunjucks
